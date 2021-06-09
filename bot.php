11b4105088373587a8dfb22cf64d6f3d638ae750
hackeranonimo2 <84729105+hackeranonimo2@users.noreply.github.com>
2021-06-09 05:59:55 -0300

    bot.php

---
+++ bot.php
@@ -0,0 +1,116 @@
+$token = '1827445667:AAE3BMRd_X5I8XOF2wSlAHIBSKRZ6gUvXV8';
+function bot($method,$datas=[]){
+  global $token;
+      $url = "https://api.telegram.org/bot".$token."/".$method;
+      $ch = curl_init();
+      curl_setopt($ch,CURLOPT_URL,$url);
+      curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+      $res = curl_exec($ch);
+      if(curl_error($ch)){
+          var_dump(curl_error($ch));
+      }else{
+          return json_decode($res);
+      }
+  }
+  
+  
+  $update = json_decode(file_get_contents('php://input'));
+
+
+$message = $update->message;
+$cid = $message->chat->id;
+$mid = $message->message_id;
+$text = $message->text;
+
+
+if($text=="/api1"){
+bot('sendMessage',[
+ 'chat_id'=>$cid,
+ 'text'=>"🔄<b>Cargando...</b>",
+ 'parse_mode'=>"HTML"
+ ]);//manba: 
+ sleep(0.3);
+bot('editMessageText',[
+ 'chat_id'=>$cid,
+ 'text'=>'⏳Cargando...
+🔆🔅🔅🔅🔅🔅🔅🔅🔅🔅 10%'
+ ]);//manba: 
+ sleep(0.5);
+bot('editMessageText',[
+ 'chat_id'=>$cid,
+ 'message_id'=>$mid +1,
+ 'text'=>'⏳Cargando...
+🔅🔆🔅🔅🔅🔅🔅🔅🔅🔅 20%'
+ ]);//manba: 
+ sleep(0.5);
+bot('editMessageText',[
+ 'chat_id'=>$cid,
+ 'message_id'=>$mid + 1,
+ 'text'=>'⏳Cargando...
+🔅🔅🔆🔅🔅🔅🔅🔅🔅🔅 30% '
+ ]);//manba: 
+ sleep(0.5);
+bot('editMessageText',[
+ 'chat_id'=>$cid,
+ 'message_id'=>$mid + 1,
+ 'text'=>'⏳Cargando...
+🔅🔅🔅🔆🔅🔅🔅🔅🔅🔅 40%'
+ ]);//manba: 
+ sleep(0.5);
+bot('editMessageText',[
+ 'chat_id'=>$cid,
+ 'message_id'=>$mid + 1,
+ 'text'=>'⏳Cargando...
+🔅🔅🔅🔅🔆🔅🔅🔅🔅🔅 50% '
+ ]);//manba: 
+ sleep(0.5);
+bot('editMessageText',[
+ 'chat_id'=>$cid,
+ 'message_id'=>$mid + 1,
+ 'text'=>'⏳Cargando...
+🔅🔅🔅🔅🔅🔆🔅🔅🔅🔅 60%'
+ ]);//manba: 
+ sleep(0.5);
+bot('editMessageText',[
+ 'chat_id'=>$cid,
+ 'message_id'=>$mid + 1,
+ 'text'=>'⏳Cargando...
+🔅🔅🔅🔅🔅🔅🔆🔅🔅🔅 70%'
+ ]);//manba: 
+ sleep(0.5);
+bot('editMessageText',[
+ 'chat_id'=>$cid,
+ 'message_id'=>$mid + 1,
+ 'text'=>'⏳Cargando...
+🔅🔅🔅🔅🔅🔅🔅🔆🔅🔅 80%'
+ ]);//manba: 
+ sleep(0.5);
+bot('editMessageText',[
+ 'chat_id'=>$cid,
+ 'message_id'=>$mid + 1,
+ 'text'=>'⏳Cargando...
+🔅🔅🔅🔅🔅🔅🔅🔅🔆🔅 90%'
+ ]);//manba: 
+ sleep(0.5);
+bot('editMessageText',[
+ 'chat_id'=>$cid,
+ 'message_id'=>$mid + 1,
+ 'text'=>'⏳Cargando...
+🔅🔅🔅🔅🔅🔅🔅🔅🔅🔆 100%'
+ ]); //manba: 
+  bot('deletemessage',[
+    'chat_id'=>$cid,
+    'message_id'=>$mid + 1,
+  ]);//manba: 
+ sleep(0.5);
+bot('sendMessage',[
+'chat_id'=>$cid,
+'text'=>"✅aqui pon\nlo que vas a mostrar✅",
+]);
+}
+
+
+
+
+https://api.telegram.org/bot1609058719:AAEKOeWPpv-lb2qeOWanDmvgtURqqIDuLLs/setWebhook?url=https://checker-logis-cental-anomoinomo.000webhostapp.com/index.php
